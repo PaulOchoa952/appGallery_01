ff2880bb8c69cfafb1d2ddf4136904c14b9429e9
paul <paalochoame@ittepic.edu.mx>
2023-12-03 10:23:34 -0700

    LoginconImagenes

--- lib/login.dart
+++ lib/login.dart
@@ -1,19 +1,29 @@
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'regisgtrar.dart';
 
-class login extends StatefulWidget {
-  const login({super.key});
-
-  @override
-  State<login> createState() => _loginState();
+void main() {
+  runApp(
+    MaterialApp(
+      home: Login(),
+    ),
+  );
 }
 
-class _loginState extends State<login> {
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GlobalKey<FormState> _forKey = GlobalKey<FormState>();
-  TextEditingController _emailCont = TextEditingController();
-  TextEditingController _contrasenaCont = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   String email = "";
   String contrasena = "";
@@ -24,79 +34,256 @@ class _loginState extends State<login> {
           email: email,
           password: contrasena
       );
-      print("Seaccedio con el usario: ${userCredential.user!.email}");
+      print("Se accedio con el usario: ${userCredential.user!.email}");
     }catch(e){
       print("Error al intentar loggearse: $e");
     }
   }
 
+  void _handleForgotPassword() {
+    // Implement your forgot password logic here
+    print("aquimero para contraseñas olvidadas");
+  }
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
-        title: Text("Login"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(30),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
           child: Form(
-              key: _forKey,
-              child: Column(
-                mainAxisAlignment:  MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: _emailCont,
-                    keyboardType:  TextInputType.text,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Email"
+            key: _formKey,
+            child:Column(
+              children: <Widget>[
+                Container(
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/b2.png'),
+                      fit: BoxFit.fill,
                     ),
-                    validator: (value){
-                      if(value == null || value.isEmpty){
-                        return "Ingrese un correo";
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      setState(() {
-                        email = value;
-                      });
-                    },
                   ),
-                  SizedBox( height:20),
-                  TextFormField(
-                    controller: _contrasenaCont,
-                    keyboardType:  TextInputType.text,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Contraseña"
-                    ),
-                    validator: (value){
-                      if(value == null || value.isEmpty){
-                        return "Ingrese la contraseña";
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      setState(() {
-                        contrasena = value;
-                      });
-                    },
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 30,
+                        width: 80,
+                        height: 200,
+                        child: FadeInUp(
+                          duration: Duration(seconds: 1),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/light-1.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 140,
+                        width: 80,
+                        height: 150,
+                        child: FadeInUp(
+                          duration: Duration(milliseconds: 1200),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/light-2.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 40,
+                        top: 40,
+                        width: 80,
+                        height: 150,
+                        child: FadeInUp(
+                          duration: Duration(milliseconds: 1300),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/evento.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        child: FadeInUp(
+                          duration: Duration(milliseconds: 1600),
+                          child: Container(
+                            margin: EdgeInsets.only(top: 50),
+                            child: Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  SizedBox( height:20),
-                  ElevatedButton(
-                      onPressed: (){
-                        if(_forKey.currentState!.validate()){
-                          _handleLogin();
-                        }
-                      },
-                      child: Text("Acceder"))
-                ],
-              )
+                ),
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    children: <Widget>[
+                      FadeInUp(
+                        duration: Duration(milliseconds: 1800),
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Color.fromRGBO(143, 148, 251, 1),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(143, 148, 251, .2),
+                                blurRadius: 20.0,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Color.fromRGBO(143, 148, 251, 1),
+                                    ),
+                                  ),
+                                ),
+                                child:
+                                TextFormField(
+                                  controller: _emailController,
+                                  keyboardType:  TextInputType.text,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Correo",
+                                    hintStyle: TextStyle(
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                  validator: (value){
+                                    if(value == null || value.isEmpty){
+                                      return "Ingrese un correo";
+                                    }
+                                    return null;
+                                  },
+                                  onChanged: (value) {
+                                    setState(() {
+                                      email = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                child: TextFormField(
+                                  controller: _passwordController,
+                                  keyboardType:  TextInputType.text,
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Contraseña",
+                                    hintStyle: TextStyle(
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                  validator: (value){
+                                    if(value == null || value.isEmpty){
+                                      return "Ingrese la contraseña";
+                                    }
+                                    return null;
+                                  },
+                                  onChanged: (value) {
+                                    setState(() {
+                                      contrasena = value;
+                                    });
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      FadeInUp(
+                        duration: Duration(milliseconds: 1900),
+                        child: Container(
+                          child: Center(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _handleLogin();
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.purpleAccent,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Text(
+                                  "Acceder",
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      FadeInUp(
+                        duration: Duration(milliseconds: 2000),
+                        child:  TextButton(
+                          onPressed: _handleForgotPassword,
+                          style: TextButton.styleFrom(
+                            primary: Colors.purpleAccent,
+                          ),
+                          child: Text("¿Olvidó su contraseña?"),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      FadeInUp(
+                        duration: Duration(milliseconds: 2000),
+                        child:  TextButton(
+                          onPressed: (){
+                            Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => registrar()),
+                            );
+                          },
+                          style: TextButton.styleFrom(
+                            primary: Colors.purpleAccent,
+                          ),
+                          child: Text("Regritarse"),
+                        ),
+
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+
           ),
         ),
       ),
-
     );
   }
 }
--- lib/regisgtrar.dart
+++ lib/regisgtrar.dart
@@ -1,29 +1,32 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login.dart';
 
-class loging extends StatefulWidget {
-  const loging({super.key});
+class registrar extends StatefulWidget {
+  const registrar({super.key});
 
   @override
-  State<loging> createState() => _logingState();
+  State<registrar> createState() => _registrarState();
 }
 
-class _logingState extends State<loging> {
+class _registrarState extends State<registrar> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GlobalKey<FormState> _forKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController _emailCont = TextEditingController();
   TextEditingController _contrasenaCont = TextEditingController();
 
   String email = "";
   String contrasena = "";
 
-  void _handleSingUp() async{
+  void _handleRegistrar() async{
     try{
       UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
           email: email,
           password: contrasena
       );
       print("Usuario registrado: ${userCredential.user!.email}");
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Login()),);
     }catch(e){
       print("Error al interntar registrar al usuario: $e");
     }
@@ -32,70 +35,214 @@ class _logingState extends State<loging> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
-        title: Text("Regristrar Usuario"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(30),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
           child: Form(
-              key: _forKey,
-              child: Column(
-                mainAxisAlignment:  MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: _emailCont,
-                    keyboardType:  TextInputType.text,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Email"
+              key: _formKey,
+              child:Column(
+                children: <Widget>[
+                  Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/background.png'),
+                        fit: BoxFit.fill,
+                      ),
                     ),
-                    validator: (value){
-                      if(value == null || value.isEmpty){
-                        return "Ingrese un correo";
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      setState(() {
-                        email = value;
-                      });
-                    },
-                  ),
-                  SizedBox( height:20),
-                  TextFormField(
-                    controller: _contrasenaCont,
-                    keyboardType:  TextInputType.text,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Contraseña"
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          left: 30,
+                          width: 80,
+                          height: 200,
+                          child: FadeInUp(
+                            duration: Duration(seconds: 1),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/light-1.png'),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 140,
+                          width: 80,
+                          height: 150,
+                          child: FadeInUp(
+                            duration: Duration(milliseconds: 1200),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/light-2.png'),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 40,
+                          top: 40,
+                          width: 80,
+                          height: 150,
+                          child: FadeInUp(
+                            duration: Duration(milliseconds: 1300),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/agregar.png'),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          child: FadeInUp(
+                            duration: Duration(milliseconds: 1600),
+                            child: Container(
+                              margin: EdgeInsets.only(top: 50),
+                              child: Center(
+                                child: Text(
+                                  "Registrarse",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                    validator: (value){
-                      if(value == null || value.isEmpty){
-                        return "Ingrese la contraseña";
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      setState(() {
-                        contrasena = value;
-                      });
-                    },
                   ),
-                  SizedBox( height:20),
-                  ElevatedButton(
-                      onPressed: (){
-                        if(_forKey.currentState!.validate()){
-                          _handleSingUp();
-                        }
-                      },
-                      child: Text("Registrar"))
+                  Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Column(
+                      children: <Widget>[
+                        FadeInUp(
+                          duration: Duration(milliseconds: 1800),
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Color.fromRGBO(143, 148, 251, 1),
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromRGBO(143, 148, 251, .2),
+                                  blurRadius: 20.0,
+                                  offset: Offset(0, 10),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(
+                                        color: Color.fromRGBO(143, 148, 251, 1),
+                                      ),
+                                    ),
+                                  ),
+                                  child:
+                                  TextFormField(
+                                    controller: _emailCont,
+                                    keyboardType:  TextInputType.text,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Correo",
+                                      hintStyle: TextStyle(
+                                        color: Colors.grey[700],
+                                      ),
+                                    ),
+                                    validator: (value){
+                                      if(value == null || value.isEmpty){
+                                        return "Ingrese un correo";
+                                      }
+                                      return null;
+                                    },
+                                    onChanged: (value) {
+                                      setState(() {
+                                        email = value;
+                                      });
+                                    },
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: TextFormField(
+                                    controller: _contrasenaCont,
+                                    keyboardType:  TextInputType.text,
+                                    obscureText: true,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Contraseña",
+                                      hintStyle: TextStyle(
+                                        color: Colors.grey[700],
+                                      ),
+                                    ),
+                                    validator: (value){
+                                      if(value == null || value.isEmpty){
+                                        return "Ingrese la contraseña";
+                                      }
+                                      return null;
+                                    },
+                                    onChanged: (value) {
+                                      setState(() {
+                                        contrasena = value;
+                                      });
+                                    },
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        FadeInUp(
+                          duration: Duration(milliseconds: 1900),
+                          child: Container(
+                            child: Center(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    _handleRegistrar();
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.deepPurpleAccent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(12.0),
+                                  child: Text(
+                                    "Registrarse",
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               )
+
           ),
         ),
       ),
-
     );
   }
 }
